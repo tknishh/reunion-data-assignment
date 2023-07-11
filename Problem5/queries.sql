a627c9c27bcc90316b9808e41d4368418936b8eb
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2023-07-11 15:55:12 +0530

    Create queries.sql

---
+++ Problem5/queries.sql
@@ -0,0 +1,15 @@
+-- SQL queries for Problem 5
+
+SELECT
+    t1.Name AS 'Bot Name',
+    t1.Start AS 'Start',
+    t2.End AS 'End',
+    GROUP_CONCAT(t1.Activity ORDER BY t1.Start SEPARATOR ', ') AS 'Activities'
+FROM
+    TimeSeries t1
+JOIN
+    TimeSeries t2 ON t1.Name = t2.Name AND t1.Start <= t2.End
+GROUP BY
+    t1.Name, t1.Start, t2.End
+ORDER BY
+    t1.Name, t1.Start
